85fefc1c446e49e39e8b1dd31bed1f4b5a6b924b
vv2dev <74323401+natsukashiiz@users.noreply.github.com>
2023-06-15 09:44:12 +0700

    ~init

---
+++ src/main/java/com/natsukashiiz/springbootjwtrsa/SpringBootJwtRsaApplication.java
@@ -0,0 +1,13 @@
+package com.natsukashiiz.springbootjwtrsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtRsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtRsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/natsukashiiz/springbootjwtrsa/SpringBootJwtRsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natsukashiiz.springbootjwtrsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtRsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
